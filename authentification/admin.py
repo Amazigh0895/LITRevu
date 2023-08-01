287cfe3711b8ceafb4c1a6f343284db0bf8037a0
Amazigh0895 <arab.fechetah@outlook.fr>
2023-08-01 08:21:34 +0200

    ajout de possiblite de gerer dans le site admin

--- authentification/admin.py
+++ authentification/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from authentification.models import UserFollows, User, Ticket, Review
 
 # Register your models here.
+
+
+admin.site.register(UserFollows)
+admin.site.register(User)
+admin.site.register(Ticket)
+admin.site.register(Review)
