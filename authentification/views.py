dbc556714b2a203b504ba82565c6da61f43e1525
Amazigh0895 <arab.fechetah@outlook.fr>
2023-08-03 13:00:15 +0200

    ajout de condition de verification de connexion

--- authentification/views.py
+++ authentification/views.py
@@ -7,26 +7,30 @@ from . import forms
 
 # Login page  function
 def login_page(request):
-    form = forms.LoginForm()
-    message = ""
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            user = authenticate(username=form.cleaned_data['username'],
-                                password=form.cleaned_data['password'])
-            if user is not None:
-                # Connecte our user if the authentication succeed
-                login(request, user)
-                #message = f'Bonjour, {user.username}! vous etes connecté'
-                return redirect('home')
-            else:
-                # Get error message if our autheticaton doesnt succeed
-                message = " identifiants invalides"
+    if request.user.is_authenticated:
+        return redirect('home')
     else:
+
         form = forms.LoginForm()
-    # Send form and message to login.html  page
-    return render(request, 'authentification/login.html',
-                  context={'from': form, 'message': message})
+        message = ""
+        if request.method == 'POST':
+            form = forms.LoginForm(request.POST)
+            if form.is_valid():
+                user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password'])
+                if user is not None:
+                    # Connecte our user if the authentication succeed
+                    login(request, user)
+                    #message = f'Bonjour, {user.username}! vous etes connecté'
+                    return redirect('home')
+                else:
+                    # Get error message if our autheticaton doesnt succeed
+                    message = " identifiants invalides"
+        else:
+            form = forms.LoginForm()
+        # Send form and message to login.html  page
+        return render(request, 'authentification/login.html',
+                    context={'from': form, 'message': message})
 
 
 # Logout user function
@@ -35,3 +39,22 @@ def logout_user(request):
     # There we logout our user
     logout(request)
     return redirect('login-page')
+
+
+# Signup fonction
+def sign_up(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    else:
+        form = forms.SignUpForm()
+        if request.method == 'POST':
+            form = forms.SignUpForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                login(request, user)
+                return redirect('home')
+        else:
+            form = forms.SignUpForm()
+        return render(request, 'authentification/signup.html',
+            {'form': form})
