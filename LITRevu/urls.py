ef2e0936d5ef5cb08c006e12412eae8c02f07dbf
Amazigh0895 <arab.fechetah@outlook.fr>
2023-08-03 13:41:11 +0200

    ajout des MEDIA aux urls

--- LITRevu/urls.py
+++ LITRevu/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 import authentification.views
 import blog.views
@@ -26,3 +28,7 @@ urlpatterns = [
     path('blog/home/', blog.views.home, name="home"),
     path('signup/', authentification.views.sign_up, name="sign-up")
 ]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
